b91fe804b9815c8b1af8e35250085ca661fe21ae
Patryk Wasilewski <pat.wasilewski04@gmail.com>
2025-05-17 07:56:40 +0200

    Added - Const that contains replaceable config for '.bashrc'.

--- main.c
+++ main.c
@@ -3,6 +3,14 @@
 #include <gtk/gtk.h> // sudo apt install libgtk-3-dev -y
 
 
+const char *TRACKING_CONFIG =
+    "\n\n#####################################################\n"
+    "# CURRENT USED - Command_Tracker - CONFIGURATION\n"
+    "#####################################################\n\n"
+    "export PROMPT_COMMAND='history -a; echo \"$(whoami)@$(hostname):$(pwd) $ $(history 1 | sed \"s/^ *[0-9]* *//\")\" >> \"$(xdg-user-dir DESKTOP)/CT_command_history.txt\"'\n\n"
+    "#####################################################\n";
+
+
 int main(int argc, char *argv[]) {
 
     return 0;
